1b6a7aff8817216b012c5eab1be705e8ff3865fc
Yusufm.salh <yusufm.salh@gmail.com>
2020-01-01 18:37:49 +0200

    read user input and show hello + input

---
+++ XamarinFormsBasics/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsBasics
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // read user input and show hello + input
+           var name = txtName.Text;
+            lblName.Text = "Hello " + name;
+
+        }
+    }
+}
